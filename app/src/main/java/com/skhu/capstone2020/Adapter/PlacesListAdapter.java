eaa12323c6b460c5d5401a9f74f12c795e8b528e
ldy2980 <ldyclank@gmail.com>
2020-05-05 19:42:11 +0900

    2020/05/05

--- app/src/main/java/com/skhu/capstone2020/Adapter/PlacesListAdapter.java
+++ app/src/main/java/com/skhu/capstone2020/Adapter/PlacesListAdapter.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.skhu.capstone2020.Model.Place;
 import com.skhu.capstone2020.R;
 
@@ -39,6 +41,13 @@ public class PlacesListAdapter extends RecyclerView.Adapter<PlacesListAdapter.Vi
             holder.place_group_category.setText(place.getGroupCategory());
             holder.place_address.setText(place.getAddress());
 
+            holder.place_main_image.setPadding(0, 0, 0, 0);
+            holder.place_main_image.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            Glide.with(context)
+                    .load("https://img1.daumcdn.net/thumb/T800x0.q70/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fplace%2F0DE4A16B703244D0A9D62143E18E6A48")
+                    .error(R.drawable.ic_default_place_image)
+                    .into(holder.place_main_image);
+
             if (!place.getPhone().isEmpty()) {
                 holder.place_digit.setVisibility(View.VISIBLE);
                 holder.place_digit.setText(place.getPhone());
@@ -58,6 +67,7 @@ public class PlacesListAdapter extends RecyclerView.Adapter<PlacesListAdapter.Vi
 
     static class ViewHolder extends RecyclerView.ViewHolder {
         TextView place_name, place_category, place_group_category, place_digit, place_address, place_distance;
+        ImageView place_main_image;
 
         ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -67,6 +77,9 @@ public class PlacesListAdapter extends RecyclerView.Adapter<PlacesListAdapter.Vi
             place_address = itemView.findViewById(R.id.place_address);
             place_distance = itemView.findViewById(R.id.place_distance);
             place_digit = itemView.findViewById(R.id.place_digit);
+
+            place_main_image = itemView.findViewById(R.id.place_main_image);
+            place_main_image.setClipToOutline(true);
         }
     }
 }
--- app/src/main/java/com/skhu/capstone2020/Fragments/SurroundingFragment.java
+++ app/src/main/java/com/skhu/capstone2020/Fragments/SurroundingFragment.java
@@ -6,9 +6,14 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -28,6 +33,10 @@ import com.skhu.capstone2020.REST_API.Client;
 import com.skhu.capstone2020.REST_API.KakaoLocalApi;
 
 import org.jetbrains.annotations.NotNull;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.util.List;
 import java.util.Objects;
@@ -42,6 +51,10 @@ public class SurroundingFragment extends Fragment {
     private KakaoLocalApi api;
     private LocationManager lm;
     private PlacesListAdapter adapter;
+
+    private WebView webView;
+    private WebSettings mWebSettings;
+    private String source;
 
     private RelativeLayout fragment_container;
 
@@ -59,6 +72,39 @@ public class SurroundingFragment extends Fragment {
         surrounding_place_recycler = view.findViewById(R.id.surrounding_place_recycler);
         surrounding_place_recycler.setHasFixedSize(true);
         surrounding_place_recycler.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        webView = view.findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient()); // 클릭시 새창 안뜨게
+        mWebSettings = webView.getSettings(); //세부 세팅 등록
+        mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스크립트 허용 여부
+        mWebSettings.setSupportMultipleWindows(false); // 새창 띄우기 허용 여부
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
+        mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
+        mWebSettings.setUseWideViewPort(true); // 화면 사이즈 맞추기 허용 여부
+        mWebSettings.setSupportZoom(false); // 화면 줌 허용 여부
+        mWebSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
+        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
+        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
+        mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
+        webView.addJavascriptInterface(new MyJavascriptInterface(), "Android");
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(final WebView view, String url) {
+                super.onPageFinished(view, url);
+                // 자바스크립트 인터페이스로 연결되어 있는 getHTML를 실행
+                // 자바스크립트 기본 메소드로 html 소스를 통째로 지정해서 인자로 넘김
+                view.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        view.loadUrl("javascript:window.Android.getHtml(document.getElementsByTagName('body')[0].innerHTML);");
+                    }
+                }, 100);
+            }
+        });
+
+        webView.loadUrl("https://place.map.kakao.com/14931853"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
 
         setList();
 
@@ -107,6 +153,29 @@ public class SurroundingFragment extends Fragment {
                         Toast.makeText(getContext(), t.getMessage(), Toast.LENGTH_LONG).show();
                     }
                 });
+    }
+
+    private void getImageUrls(String sourceUrl) {                                                                   // 이미지 URL 크롤링
+
+    }
+
+    public class MyJavascriptInterface {
+        @JavascriptInterface
+        public void getHtml(String html) {
+            //위 자바스크립트가 호출되면 여기로 html이 반환됨
+            source = html;
+            Document document = Jsoup.parse(html);
+            Elements elements = document.select(".details_present").select("a");            // 이미지 URL이 있는 태그 선택
+/*            Log.d("WebView Test", "getHtml: " + source);
+            Log.d("WebView Test", "Count: " + elements.size() + elements.toString());*/
+
+            if (elements.size() != 0)
+                for (Element element : elements) {
+                    String url = element.attr("style");
+                    String parsedUrl = "https:" + url.substring(23, url.length() - 19);
+                    Log.d("WebView Test", "URL: " + parsedUrl);
+                }
+        }
     }
 
     private PermissionListener permissionlistener = new PermissionListener() {
